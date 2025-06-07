5def072d98cf62ff3308306e7fdcb7e05604356d
M.Aoun Abdullah <aoun84521@gmail.com>
2025-06-07 10:46:17 +0500

    ...

--- Day25.py
+++ Day25.py
@@ -1,22 +1,22 @@
 # import csv
 import pandas as pd
-
 # with open('weather_data.csv') as file:
 #     # lines = file.read()
 #     # print(lines)
 #     data = csv.reader(file)
 #     temperature = []
-#     for row in data:
+#     for row in data:P
 #         if row[1] != 'temp':
 #             temperature.append(int(row[1]))
 
+
 # print(temperature)
 
-df = pd.read_csv('weather_data.csv')
+# df = pd.read_csv('weather_data.csv')
 
-data_dict = df.to_dict()
-temp = df['temp']
-temp_list = df['temp'].to_list()
+# data_dict = df.to_dict()
+# temp = df['temp']
+# temp_list = df['temp'].to_list()
 
 # sum_Temp = 0
 # count = 0
@@ -32,7 +32,9 @@ temp_list = df['temp'].to_list()
 # print(round(avg_temp, 2))
 
 # Get Data in row
-print(df[df['temp'].max() == df.temp])
+# print(df[df['temp'].max() == df.temp])
+
+df = pd.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20250607.csv')
 
 
 
